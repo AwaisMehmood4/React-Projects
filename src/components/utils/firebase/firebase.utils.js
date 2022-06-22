de61923b668888bf58bd39524abc76f606c3b997
Hafiz Awais <awaismahmood4@gmail.com>
2022-06-22 13:11:25 +0500

    added categories collection and data in firestore

--- src/components/utils/firebase/firebase.utils.js
+++ src/components/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, writeBatch} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBFEG9Uqiz6B5tFMhfexHsdodgUDuEjZEk",
@@ -24,6 +24,20 @@ export const signInWithGooglePoopup = () => signInWithPopup(auth, googleProvider
 export const signInWithGooglRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+
+export const addCollectionAndDocuments = async (collectionKey, objectToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectToAdd.forEach((object) => {
+        const docRef = doc(collectionRef,object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('done');
+}
  
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {})=> {
 
